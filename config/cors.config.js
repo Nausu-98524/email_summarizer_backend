7b0aa9d53868406750402e8e2717fa646cd668df
Nausu-98524 <naushadmech18@gmail.com>
2026-02-05 22:48:22 +0530

    added frontend url in cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -7,20 +7,20 @@ export const configureCors = () => {
       console.log("[CORS] incoming Origin:", origin);
 
       const allowedOrigins = [
-        "http://localhost:5173", // React dev server
+        //"http://localhost:5173", // React dev server
+        "https://email-ummarizer-frontend-zdh1.vercel.app/", // Production
       ];
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true); 
+        callback(null, true);
       } else {
         callback(new Error("Not allowed by cors"));
       }
     },
-    methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept-Version"],
     exposedHeaders: ["X-Total-Count", "Content-Range"],
-    credentials: true, 
+    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
 };
-
